92d6dde887394f722d5a3e5a7d406b5bf548a936
Anna <annapn5@mail.ru>
2023-01-21 10:05:26 +0600

    домашняя после 15.01 урока

---
+++ zadanie2/Program.cs
@@ -0,0 +1,41 @@
+﻿/*Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на чётных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0*/
+void FillArr(int []arr)
+{
+    for(int i = 0; i<arr.Length; i++)
+    {
+        arr [i] = new Random().Next(1,1000);
+    }
+}
+void Printarr (int []arr)
+{
+    for(int i = 0; i<arr.Length; i++)
+    {
+        Console.WriteLine("I Равно: " + i);
+        Console.WriteLine(arr[i]);
+    }
+}
+void SumOfEvenNum(int[]col)
+{
+    int i = 0;
+    int result = 0;
+    while (i<col.Length)
+        {
+            if(i%2==0)
+        {
+            result = result + col[i];
+        }
+        i++;
+        }
+    Console.WriteLine($"Сумма элементов, стоящих на четных позициях: {result}"); 
+}        
+       
+
+int[]col = new int [10];
+FillArr (col);
+Printarr (col);
+SumOfEvenNum(col);
---
+++ zadanie3/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76*/
+int MinOfNumbers(int[] col)
+{ int min = col[0];
+  for (int i=0; i<col.Length; i++)
+  {
+    if(min>col[i])
+    {
+        min=col[i];
+    }
+  }
+  Console.WriteLine("Минимальное число массива " + min);
+  return(min);
+}
+int MaxOfNumbers(int[] kol)
+{ int max = kol[0];
+  for (int i=0; i<kol.Length; i++)
+  {
+    if(max<kol[i])
+    {
+        max=kol[i];
+    }
+  }
+  Console.WriteLine("Максимальное число массива " + max);
+  return(max);
+}
+void DiffOfMaxAndMin(int h1, int h2)
+{
+    int diff = h1 - h2;
+    Console.WriteLine( "Разница между максимальным и минимальным числами массива " + diff);
+}
+
+      int [] col = {234,567, 76, 900, 786};
+      DiffOfMaxAndMin(MaxOfNumbers(col), MinOfNumbers(col));
+      
